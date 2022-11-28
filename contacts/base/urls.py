a5b85c37b81bc6c6661e2a7134f69a9c05d8a829
Jim Floyd <jamshid.elboev5111995@gmail.com>
2022-11-28 16:27:35 +0500

    finished

--- contacts/base/urls.py
+++ contacts/base/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.contacts, name='contacts'),
-    path('/add_contact', views.add_contact, name='add_contact')
+    path('add_contact/', views.add_contact, name='add_contact'),
+    path('add_contact/save_contact', views.save_contact, name='save_contact'),
+    path('update_form/<str:pk>', views.update_form, name='update_form'),
+    path('update_contact/<str:pk>',
+         views.update_contact, name='update_contact'),
+    path('delete_contact/<str:pk>', views.delete_contact, name='delete_contact'),
 ]
--- contacts/base/views.py
+++ contacts/base/views.py
@@ -1,10 +1,47 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Contact
+from .form import ContactForm
+from django.contrib import messages
+
 
 def contacts(request):
     contacts = Contact.objects.all()
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'contacts': contacts})
+
 
 def add_contact(request):
-    return render(request, 'add_contact.html')
+    return render(request, 'contact_form.html')
+
+
+def save_contact(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        phone_number = request.POST.get('phone_number')
+        try:
+            if len(phone_number) == 9 and int(phone_number):
+                contact = Contact.objects.create(
+                    username=username, number=phone_number)
+                return redirect('contacts')
+        except:
+            messages.add_message(
+                request, messages.INFO, 'Telefon raqami 9 ta sondan iborat bo`lishi lozim')
+            return redirect('add_contact')
+
+
+def update_form(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, 'update_form.html', {'contact': contact})
+
+
+def update_contact(request, pk):
+    username = request.POST.get('username')
+    phone_number = request.POST.get('phone_number')
+    Contact.objects.filter(id=pk).update(
+        username=username, number=phone_number)
+    return redirect('contacts')
+
+
+def delete_contact(request, pk):
+    Contact.objects.filter(id=pk).delete()
+    return redirect('contacts')

b81df3436376c265e3ed894b6af0c3846501f55c
Jim Floyd <jamshid.elboev5111995@gmail.com>
2022-12-11 10:49:02 +0500

    pagination2

--- contacts/base/views.py
+++ contacts/base/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 
 
 cont_num_in_page = 4
-contact_len = len(Contact.objects.all())
 
 
 def contacts(request):
@@ -16,6 +15,31 @@ def contacts(request):
         contacts = Contact.objects.all()
         return render(request, 'home.html', {'contacts': contacts, 'contact_len': contact_len, 'cont_num_in_page': cont_num_in_page})
     return redirect('pagination', 1)
+
+
+contact_len = len(Contact.objects.all())
+
+
+def pagination(request, pk):
+    if contact_len % cont_num_in_page != 0:
+        all_page_num = contact_len//cont_num_in_page+1
+        if pk < all_page_num:
+            contacts_in_page = Contact.objects.all(
+            )[cont_num_in_page*(pk-1):cont_num_in_page*pk]
+        else:
+            contacts_in_page = Contact.objects.all(
+            )[cont_num_in_page*(pk-1):contact_len]
+    else:
+        all_page_num = contact_len//cont_num_in_page
+        contacts_in_page = Contact.objects.all(
+        )[cont_num_in_page*(pk-1):cont_num_in_page*pk]
+
+    print(all_page_num)
+    print(contact_len)
+    print(cont_num_in_page)
+
+    return render(request, 'home.html', {'contacts': contacts, 'contacts_in_page': contacts_in_page, 'n': range(1, all_page_num+1)})
+
 
 # Kontakt qo'shish uchun ma'lumot to'ldiriladigan sahifa
 
@@ -69,24 +93,3 @@ def update_contact(request, pk):
 def delete_contact(request, pk):
     Contact.objects.filter(id=pk).delete()
     return redirect('contacts')
-
-
-def pagination(request, pk):
-    if contact_len % cont_num_in_page != 0:
-        all_page_num = contact_len//cont_num_in_page+1
-        if pk < all_page_num:
-            contacts_in_page = Contact.objects.all(
-            )[cont_num_in_page*(pk-1):cont_num_in_page*pk]
-        else:
-            contacts_in_page = Contact.objects.all(
-            )[cont_num_in_page*(pk-1):contact_len]
-    else:
-        all_page_num = contact_len//cont_num_in_page
-        contacts_in_page = Contact.objects.all(
-        )[cont_num_in_page*(pk-1):cont_num_in_page*pk]
-
-    print(all_page_num)
-    print(contact_len)
-    print(cont_num_in_page)
-
-    return render(request, 'home.html', {'contacts': contacts, 'contacts_in_page': contacts_in_page, 'n': range(1, all_page_num+1)})

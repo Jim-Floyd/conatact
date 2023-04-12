e8882566a0dae505cefb0880f769e9190159cf02
Jim Floyd <jamshid.elboev5111995@gmail.com>
2023-04-12 17:17:23 +0500

    vercel added

--- contacts/contacts/settings.py
+++ contacts/contacts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m+if^o9n2%sssp-e@-q(!+q=f^r-3yhd$zodlx!&&c&dry2=*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -118,9 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
